cb08ea4427f2fe18408617ec23e4597a7bb2a9f3
drmiller <drmiller>
2011-05-06 18:32:16 +0000

    rearrange code for sub-keyboard modules

--- w600-sim/w600_fe.java
+++ w600-sim/w600_fe.java
@@ -38,6 +38,9 @@ class _KeyArray {
 	static final int MODE1_CHG(int b) {
 		return (0x0300 | b);
 	}
+	static final int META_KEY(int b) {
+		return (0x0400 | b);
+	}
 
 	String icon;
 	Color color;
@@ -63,7 +66,7 @@ public class w600_fe {
 
 		front_end.getContentPane().setBackground(Color.black);
 		front_end.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		front_end.setSize(1000,300);
+		front_end.setSize(1000,500);
 		front_end.setVisible(true);
 	}
 }
@@ -72,12 +75,127 @@ class Wang600_Keyboard extends JComponent
 	implements ActionListener
 {
 	static final long serialVersionUID = 31145769203L;
-	static final int num_keys = 54;
+	static final int num_kbds = 1;
 
 	GridBagLayout gridbag = new GridBagLayout();
+	int _nkbds;
+	Wang600_Keyboards[] _kbds;
+	int _row;
+	int _col;
+
+	public Wang600_Keyboard() {
+		int x;
+		_kbds = new Wang600_Keyboards[num_kbds];
+		_nkbds = 0;
+		_row = 0;
+		_col = 0;
+		Dimension dim = new Dimension(500, 25);
+		GridBagConstraints s = new GridBagConstraints();
+		JSeparator sep;
+
+		s.fill = GridBagConstraints.NONE;
+		s.gridx = 0;
+		s.gridy = 0;
+		s.weightx = 0;
+		s.weighty = 0;
+		s.gridwidth = 1;
+		s.gridheight = 1;
+
+		setLayout(gridbag);
+
+//		addButton(s,
+//			new _KeyArray("icons/k00.gif",_KeyArray.white1, 1, 1, 0, 0, _KeyArray.META_KEY(0)));
+//		addButton(s,
+//			new _KeyArray("icons/k01.gif",_KeyArray.white1, 1, 1, 1, 0, _KeyArray.META_KEY(1)));
+//		addButton(s,
+//			new _KeyArray("icons/k02.gif",_KeyArray.white1, 1, 1, 2, 0, _KeyArray.META_KEY(2)));
+//		addButton(s,
+//			new _KeyArray("icons/k03.gif",_KeyArray.white1, 1, 1, 3, 0, _KeyArray.META_KEY(3)));
+//		addButton(s,
+//			new _KeyArray("icons/k04.gif",_KeyArray.white1, 1, 1, 4, 0, _KeyArray.META_KEY(4)));
+//		addButton(s,
+//			new _KeyArray("icons/k05.gif",_KeyArray.white1, 1, 1, 5, 0, _KeyArray.META_KEY(5)));
+//		addButton(s,
+//			new _KeyArray("icons/k06.gif",_KeyArray.white1, 1, 1, 6, 0, _KeyArray.META_KEY(6)));
+//		addButton(s,
+//			new _KeyArray("icons/k07.gif",_KeyArray.white1, 1, 1, 7, 0, _KeyArray.META_KEY(7)));
+//		addButton(s,
+//			new _KeyArray("icons/k08.gif",_KeyArray.white1, 1, 1, 8, 0, _KeyArray.META_KEY(8)));
+//		addButton(s,
+//			new _KeyArray("icons/k09.gif",_KeyArray.white1, 1, 1, 9, 0, _KeyArray.META_KEY(9)));
+//		addButton(s,
+//			new _KeyArray("icons/k10.gif",_KeyArray.white1, 1, 1, 10, 0, _KeyArray.META_KEY(10)));
+//		addButton(s,
+//			new _KeyArray("icons/k11.gif",_KeyArray.white1, 1, 1, 11, 0, _KeyArray.META_KEY(11)));
+//		addButton(s,
+//			new _KeyArray("icons/k12.gif",_KeyArray.white1, 1, 1, 12, 0, _KeyArray.META_KEY(12)));
+//		addButton(s,
+//			new _KeyArray("icons/k13.gif",_KeyArray.white1, 1, 1, 13, 0, _KeyArray.META_KEY(13)));
+//		addButton(s,
+//			new _KeyArray("icons/k14.gif",_KeyArray.white1, 1, 1, 14, 0, _KeyArray.META_KEY(14)));
+//		addButton(s,
+//			new _KeyArray("icons/k15.gif",_KeyArray.white1, 1, 1, 15, 0, _KeyArray.META_KEY(15)));
+//		_col = 0;
+//		_row += 1;
+
+		s.gridx = _col;
+		s.gridy = _row;
+		sep = new JSeparator(SwingConstants.HORIZONTAL);
+		sep.setPreferredSize(dim);
+		gridbag.setConstraints(sep, s);
+		add(sep);
+		_col = 0;
+		_row += 1;
+
+		Wang600_Keyboards kbd;
+
+		kbd = new Wang600_Keyboard_main();
+		for (x = 0; x < kbd._nkeys; ++x) {
+			kbd._buttons[x].addActionListener(this);
+		}
+		gridbag.setConstraints(kbd, s);
+		add(kbd);
+		_kbds[_nkbds] = kbd;
+		++_nkbds;
+	}
+
+	public void actionPerformed(ActionEvent e) {
+		int x, y;
+		for (y = 0; y < _nkbds; ++y) {
+			for (x = 0; x < _kbds[y]._keys.length; ++x) {
+				if (e.getSource() == _kbds[y]._buttons[x]) {
+					if (_kbds[y]._keys[x].code < 0) {
+						System.out.println(_kbds[y]._keys[x].code);
+					} else {
+						int h = _kbds[y]._keys[x].code >> 4;
+						int l = _kbds[y]._keys[x].code & 0x0f;
+						System.out.format("%02d %02d\n", h, l);
+					}
+					break;
+				}
+			}
+		}
+	}
+}
+
+class Wang600_Keyboards extends JComponent
+{
+	static final long serialVersionUID = 311457692032L;
+	public Wang600_Keyboards() { }
+
 	int _nkeys;
 	_KeyArray[] _keys;
 	JButton[] _buttons;
+}
+
+class Wang600_Keyboard_main extends Wang600_Keyboards
+{
+	static final long serialVersionUID = 311457692031L;
+	static final int num_keys = 54;
+
+	GridBagLayout gridbag = new GridBagLayout();
+	int _row;
+	int _col;
 
 	private void addButton(GridBagConstraints c, _KeyArray key) {
 		final Insets inset = new Insets(2,2,2,2);
@@ -97,21 +215,22 @@ class Wang600_Keyboard extends JComponent
 
 		c.gridwidth = key.lx;
 		c.gridheight = key.ly;
-		c.gridx = key.px;
-		c.gridy = key.py;
+		c.gridx = _col + key.px;
+		c.gridy = _row + key.py;
 		gridbag.setConstraints(butt, c);
 
 		add(butt);
-		butt.addActionListener(this);
 		_buttons[_nkeys] = butt;
 		_keys[_nkeys] = key;
 		++_nkeys;
 	}
 
-	public Wang600_Keyboard() {
+	public Wang600_Keyboard_main() {
 		_buttons = new JButton[num_keys];
 		_keys = new _KeyArray[num_keys];
 		_nkeys = 0;
+		_row = 0;
+		_col = 0;
 		Dimension dim = new Dimension(25, 200);
 		GridBagConstraints c = new GridBagConstraints();
 		GridBagConstraints s = new GridBagConstraints();
@@ -142,151 +261,155 @@ class Wang600_Keyboard extends JComponent
 			new _KeyArray("icons/rad_deg.gif",_KeyArray.green1,1, 1, 0, 1, _KeyArray.PROG_CODE(8,9)));
 		addButton(c,
 			new _KeyArray("icons/shift.gif",_KeyArray.white1, 1, 2, 0, 2, _KeyArray.SHIFT));
+		++_col;
 		addButton(c,
-			new _KeyArray("icons/sin.gif",	_KeyArray.green1, 1, 1, 1, 0, _KeyArray.PROG_CODE(8,6)));
+			new _KeyArray("icons/sin.gif",	_KeyArray.green1, 1, 1, 0, 0, _KeyArray.PROG_CODE(8,6)));
 		addButton(c,
-			new _KeyArray("icons/tan.gif",	_KeyArray.green1, 1, 1, 1, 1, _KeyArray.PROG_CODE(8,8)));
+			new _KeyArray("icons/tan.gif",	_KeyArray.green1, 1, 1, 0, 1, _KeyArray.PROG_CODE(8,8)));
 		addButton(c,
-			new _KeyArray("icons/logex.gif",_KeyArray.green1, 1, 1, 1, 2, _KeyArray.PROG_CODE(8,10)));
+			new _KeyArray("icons/logex.gif",_KeyArray.green1, 1, 1, 0, 2, _KeyArray.PROG_CODE(8,10)));
 		addButton(c,
-			new _KeyArray("icons/x2.gif",	_KeyArray.green1, 1, 1, 1, 3, _KeyArray.PROG_CODE(8,12)));
+			new _KeyArray("icons/x2.gif",	_KeyArray.green1, 1, 1, 0, 3, _KeyArray.PROG_CODE(8,12)));
+		++_col;
 		addButton(c,
-			new _KeyArray("icons/cos.gif",	_KeyArray.green1, 1, 1, 2, 0, _KeyArray.PROG_CODE(8,7)));
+			new _KeyArray("icons/cos.gif",	_KeyArray.green1, 1, 1, 0, 0, _KeyArray.PROG_CODE(8,7)));
 		addButton(c,
-			new _KeyArray("icons/inv.gif",	_KeyArray.green1, 1, 1, 2, 1, _KeyArray.PROG_CODE(8,15)));
+			new _KeyArray("icons/inv.gif",	_KeyArray.green1, 1, 1, 0, 1, _KeyArray.PROG_CODE(8,15)));
 		addButton(c,
-			new _KeyArray("icons/ex.gif",	_KeyArray.green1, 1, 1, 2, 2, _KeyArray.PROG_CODE(8,11)));
+			new _KeyArray("icons/ex.gif",	_KeyArray.green1, 1, 1, 0, 2, _KeyArray.PROG_CODE(8,11)));
 		addButton(c,
-			new _KeyArray("icons/sqrt.gif",	_KeyArray.green1, 1, 1, 2, 3, _KeyArray.PROG_CODE(8,13)));
+			new _KeyArray("icons/sqrt.gif",	_KeyArray.green1, 1, 1, 0, 3, _KeyArray.PROG_CODE(8,13)));
+		++_col;
 
-		s.gridx = 3;
+		s.gridx = _col;
 		sep = new JSeparator(SwingConstants.VERTICAL);
 		sep.setPreferredSize(dim);
 		gridbag.setConstraints(sep, s);
 		add(sep);
+		++_col;
 
 		addButton(c,
-			new _KeyArray("icons/total.gif",_KeyArray.blue1, 1, 1, 4, 0, _KeyArray.PROG_CODE(1,15)));
+			new _KeyArray("icons/total.gif",_KeyArray.blue1, 1, 1, 0, 0, _KeyArray.PROG_CODE(1,15)));
 		addButton(c,
-			new _KeyArray("icons/div.gif",	_KeyArray.blue1, 1, 1, 4, 1, _KeyArray.PROG_CODE(5,15)));
+			new _KeyArray("icons/div.gif",	_KeyArray.blue1, 1, 1, 0, 1, _KeyArray.PROG_CODE(5,15)));
 		addButton(c,
-			new _KeyArray("icons/mult.gif",	_KeyArray.blue1, 1, 1, 4, 2, _KeyArray.PROG_CODE(4,15)));
+			new _KeyArray("icons/mult.gif",	_KeyArray.blue1, 1, 1, 0, 2, _KeyArray.PROG_CODE(4,15)));
 		addButton(c,
-			new _KeyArray("icons/store.gif",_KeyArray.blue1, 1, 1, 4, 3, _KeyArray.PROG_CODE(6,15)));
+			new _KeyArray("icons/store.gif",_KeyArray.blue1, 1, 1, 0, 3, _KeyArray.PROG_CODE(6,15)));
+		++_col;
 		addButton(c,
-			new _KeyArray("icons/minus.gif",_KeyArray.white1, 1, 1, 5, 0, _KeyArray.PROG_CODE(3,15)));
+			new _KeyArray("icons/minus.gif",_KeyArray.white1, 1, 1, 0, 0, _KeyArray.PROG_CODE(3,15)));
 		addButton(c,
-			new _KeyArray("icons/plus.gif",	_KeyArray.white1, 1, 2, 5, 1, _KeyArray.PROG_CODE(2,15)));
+			new _KeyArray("icons/plus.gif",	_KeyArray.white1, 1, 2, 0, 1, _KeyArray.PROG_CODE(2,15)));
 		addButton(c,
-			new _KeyArray("icons/recall.gif",_KeyArray.blue1, 1, 1, 5, 3, _KeyArray.PROG_CODE(7,15)));
+			new _KeyArray("icons/recall.gif",_KeyArray.blue1, 1, 1, 0, 3, _KeyArray.PROG_CODE(7,15)));
+		++_col;
 
-		s.gridx = 6;
+		s.gridx = _col;
 		sep = new JSeparator(SwingConstants.VERTICAL);
 		sep.setPreferredSize(dim);
 		gridbag.setConstraints(sep, s);
 		add(sep);
+		++_col;
 
 		addButton(c,
-			new _KeyArray("icons/chg_sign.gif",_KeyArray.green1, 1, 1, 7, 0, _KeyArray.PROG_CODE(0,12)));
+			new _KeyArray("icons/chg_sign.gif",_KeyArray.green1, 1, 1, 0, 0, _KeyArray.PROG_CODE(0,12)));
 		addButton(c,
-			new _KeyArray("icons/clear_disp.gif",_KeyArray.green1, 1, 1, 7, 1, _KeyArray.PROG_CODE(0,15)));
+			new _KeyArray("icons/clear_disp.gif",_KeyArray.green1, 1, 1, 0, 1, _KeyArray.PROG_CODE(0,15)));
 		addButton(c,
-			new _KeyArray("icons/set_exp.gif",_KeyArray.green1, 1, 1, 7, 2, _KeyArray.PROG_CODE(0,11)));
+			new _KeyArray("icons/set_exp.gif",_KeyArray.green1, 1, 1, 0, 2, _KeyArray.PROG_CODE(0,11)));
 		addButton(c,
-			new _KeyArray("icons/zero.gif",	_KeyArray.white1, 2, 1, 7, 3, _KeyArray.PROG_CODE(0,0)));
+			new _KeyArray("icons/zero.gif",	_KeyArray.white1, 2, 1, 0, 3, _KeyArray.PROG_CODE(0,0)));
+		++_col;
 		addButton(c,
-			new _KeyArray("icons/seven.gif",_KeyArray.white1, 1, 1, 8, 0, _KeyArray.PROG_CODE(0,7)));
+			new _KeyArray("icons/seven.gif",_KeyArray.white1, 1, 1, 0, 0, _KeyArray.PROG_CODE(0,7)));
 		addButton(c,
-			new _KeyArray("icons/four.gif",	_KeyArray.white1, 1, 1, 8, 1, _KeyArray.PROG_CODE(0,4)));
+			new _KeyArray("icons/four.gif",	_KeyArray.white1, 1, 1, 0, 1, _KeyArray.PROG_CODE(0,4)));
 		addButton(c,
-			new _KeyArray("icons/one.gif",	_KeyArray.white1, 1, 1, 8, 2, _KeyArray.PROG_CODE(0,1)));
+			new _KeyArray("icons/one.gif",	_KeyArray.white1, 1, 1, 0, 2, _KeyArray.PROG_CODE(0,1)));
+		++_col;
 		addButton(c,
-			new _KeyArray("icons/eight.gif",_KeyArray.white1, 1, 1, 9, 0, _KeyArray.PROG_CODE(0,8)));
+			new _KeyArray("icons/eight.gif",_KeyArray.white1, 1, 1, 0, 0, _KeyArray.PROG_CODE(0,8)));
 		addButton(c,
-			new _KeyArray("icons/five.gif",	_KeyArray.white1, 1, 1, 9, 1, _KeyArray.PROG_CODE(0,5)));
+			new _KeyArray("icons/five.gif",	_KeyArray.white1, 1, 1, 0, 1, _KeyArray.PROG_CODE(0,5)));
 		addButton(c,
-			new _KeyArray("icons/two.gif",	_KeyArray.white1, 1, 1, 9, 2, _KeyArray.PROG_CODE(0,2)));
+			new _KeyArray("icons/two.gif",	_KeyArray.white1, 1, 1, 0, 2, _KeyArray.PROG_CODE(0,2)));
 		addButton(c,
-			new _KeyArray("icons/dp.gif",	_KeyArray.white1, 2, 1, 9, 3, _KeyArray.PROG_CODE(0,10)));
+			new _KeyArray("icons/dp.gif",	_KeyArray.white1, 2, 1, 0, 3, _KeyArray.PROG_CODE(0,10)));
+		++_col;
 		addButton(c,
-			new _KeyArray("icons/nine.gif",	_KeyArray.white1, 1, 1, 10, 0, _KeyArray.PROG_CODE(0,9)));
+			new _KeyArray("icons/nine.gif",	_KeyArray.white1, 1, 1, 0, 0, _KeyArray.PROG_CODE(0,9)));
 		addButton(c,
-			new _KeyArray("icons/six.gif",	_KeyArray.white1, 1, 1, 10, 1, _KeyArray.PROG_CODE(0,6)));
+			new _KeyArray("icons/six.gif",	_KeyArray.white1, 1, 1, 0, 1, _KeyArray.PROG_CODE(0,6)));
 		addButton(c,
-			new _KeyArray("icons/three.gif",_KeyArray.white1, 1, 1, 10, 2, _KeyArray.PROG_CODE(0,3)));
+			new _KeyArray("icons/three.gif",_KeyArray.white1, 1, 1, 0, 2, _KeyArray.PROG_CODE(0,3)));
+		++_col;
 
-		s.gridx = 11;
+		s.gridx = _col;
 		sep = new JSeparator(SwingConstants.VERTICAL);
 		sep.setPreferredSize(dim);
 		gridbag.setConstraints(sep, s);
 		add(sep);
+		++_col;
 
 		addButton(c,
-			new _KeyArray("icons/minus.gif",_KeyArray.white1, 1, 1, 12, 0, _KeyArray.PROG_CODE(3,14)));
+			new _KeyArray("icons/minus.gif",_KeyArray.white1, 1, 1, 0, 0, _KeyArray.PROG_CODE(3,14)));
 		addButton(c,
-			new _KeyArray("icons/plus.gif",	_KeyArray.white1, 1, 2, 12, 1, _KeyArray.PROG_CODE(2,14)));
+			new _KeyArray("icons/plus.gif",	_KeyArray.white1, 1, 2, 0, 1, _KeyArray.PROG_CODE(2,14)));
 		addButton(c,
-			new _KeyArray("icons/recall.gif",_KeyArray.blue1, 1, 1, 12, 3, _KeyArray.PROG_CODE(7,14)));
+			new _KeyArray("icons/recall.gif",_KeyArray.blue1, 1, 1, 0, 3, _KeyArray.PROG_CODE(7,14)));
+		++_col;
 		addButton(c,
-			new _KeyArray("icons/total.gif",_KeyArray.blue1, 1, 1, 13, 0, _KeyArray.PROG_CODE(1,14)));
+			new _KeyArray("icons/total.gif",_KeyArray.blue1, 1, 1, 0, 0, _KeyArray.PROG_CODE(1,14)));
 		addButton(c,
-			new _KeyArray("icons/div.gif",	_KeyArray.blue1, 1, 1, 13, 1, _KeyArray.PROG_CODE(5,14)));
+			new _KeyArray("icons/div.gif",	_KeyArray.blue1, 1, 1, 0, 1, _KeyArray.PROG_CODE(5,14)));
 		addButton(c,
-			new _KeyArray("icons/mult.gif",	_KeyArray.blue1, 1, 1, 13, 2, _KeyArray.PROG_CODE(4,14)));
+			new _KeyArray("icons/mult.gif",	_KeyArray.blue1, 1, 1, 0, 2, _KeyArray.PROG_CODE(4,14)));
 		addButton(c,
-			new _KeyArray("icons/store.gif",_KeyArray.blue1, 1, 1, 13, 3, _KeyArray.PROG_CODE(6,14)));
+			new _KeyArray("icons/store.gif",_KeyArray.blue1, 1, 1, 0, 3, _KeyArray.PROG_CODE(6,14)));
+		++_col;
 
-		s.gridx = 14;
+		s.gridx = _col;
 		sep = new JSeparator(SwingConstants.VERTICAL);
 		sep.setPreferredSize(dim);
 		gridbag.setConstraints(sep, s);
 		add(sep);
+		++_col;
 
 		addButton(c,
-			new _KeyArray("icons/ld_prog.gif",_KeyArray.orange1, 1, 1, 15, 0, _KeyArray.PROG_CODE(8,14)));
+			new _KeyArray("icons/ld_prog.gif",_KeyArray.orange1, 1, 1, 0, 0, _KeyArray.PROG_CODE(8,14)));
 		addButton(c,
-			new _KeyArray("icons/search.gif",_KeyArray.green1, 1, 1, 15, 1, _KeyArray.PROG_CODE(8,0)));
+			new _KeyArray("icons/search.gif",_KeyArray.green1, 1, 1, 0, 1, _KeyArray.PROG_CODE(8,0)));
 		addButton(c,
-			new _KeyArray("icons/go.gif",	_KeyArray.white1, 1, 2, 15, 2, _KeyArray.PROG_CODE(8,3)));
+			new _KeyArray("icons/go.gif",	_KeyArray.white1, 1, 2, 0, 2, _KeyArray.PROG_CODE(8,3)));
+		++_col;
 		addButton(c,
-			new _KeyArray("icons/jif0.gif",	_KeyArray.green1, 1, 1, 16, 0, _KeyArray.PROG_CODE(8,4)));
+			new _KeyArray("icons/jif0.gif",	_KeyArray.green1, 1, 1, 0, 0, _KeyArray.PROG_CODE(8,4)));
 		addButton(c,
-			new _KeyArray("icons/jifplus.gif",_KeyArray.green1, 1, 1, 16, 1, _KeyArray.PROG_CODE(8,5)));
+			new _KeyArray("icons/jifplus.gif",_KeyArray.green1, 1, 1, 0, 1, _KeyArray.PROG_CODE(8,5)));
 		addButton(c,
-			new _KeyArray("icons/recall_xx.gif",_KeyArray.green1, 1, 1, 16, 2, _KeyArray.PROG_CODE(8,1)));
+			new _KeyArray("icons/recall_xx.gif",_KeyArray.green1, 1, 1, 0, 2, _KeyArray.PROG_CODE(8,1)));
 		addButton(c,
-			new _KeyArray("icons/print.gif",_KeyArray.green1, 1, 1, 16, 3, _KeyArray.PROG_CODE(8,2)));
+			new _KeyArray("icons/print.gif",_KeyArray.green1, 1, 1, 0, 3, _KeyArray.PROG_CODE(8,2)));
+		++_col;
 		addButton(c,
-			new _KeyArray("icons/i_o.gif",	_KeyArray.pink1, 1, 1, 17, 0, _KeyArray.PROG_CODE(15,0)));
+			new _KeyArray("icons/i_o.gif",	_KeyArray.pink1, 1, 1, 0, 0, _KeyArray.PROG_CODE(15,0)));
 		addButton(c,
-			new _KeyArray("icons/group1.gif",_KeyArray.pink1, 1, 1, 17, 1, _KeyArray.PROG_CODE(15,1)));
+			new _KeyArray("icons/group1.gif",_KeyArray.pink1, 1, 1, 0, 1, _KeyArray.PROG_CODE(15,1)));
 		addButton(c,
-			new _KeyArray("icons/group2.gif",_KeyArray.pink1, 1, 1, 17, 2, _KeyArray.PROG_CODE(15,2)));
+			new _KeyArray("icons/group2.gif",_KeyArray.pink1, 1, 1, 0, 2, _KeyArray.PROG_CODE(15,2)));
 		addButton(c,
-			new _KeyArray("icons/indir.gif",_KeyArray.orange1, 1, 1, 17, 3, _KeyArray.PROG_CODE(15,3)));
+			new _KeyArray("icons/indir.gif",_KeyArray.orange1, 1, 1, 0, 3, _KeyArray.PROG_CODE(15,3)));
+		++_col;
 		addButton(c,
-			new _KeyArray("icons/set_pc.gif",_KeyArray.green1, 1, 1, 18, 0, _KeyArray.SPCL_KEY(2)));
+			new _KeyArray("icons/set_pc.gif",_KeyArray.green1, 1, 1, 0, 0, _KeyArray.SPCL_KEY(2)));
 		addButton(c,
-			new _KeyArray("icons/verif_prog.gif",_KeyArray.green1, 1, 1, 18, 1, _KeyArray.SPCL_KEY(1)));
+			new _KeyArray("icons/verif_prog.gif",_KeyArray.green1, 1, 1, 0, 1, _KeyArray.SPCL_KEY(1)));
 		addButton(c,
-			new _KeyArray("icons/rec_prog.gif",_KeyArray.orange1, 1, 1, 18, 2, _KeyArray.SPCL_KEY(3)));
+			new _KeyArray("icons/rec_prog.gif",_KeyArray.orange1, 1, 1, 0, 2, _KeyArray.SPCL_KEY(3)));
 		addButton(c,
-			new _KeyArray("icons/step.gif",	_KeyArray.green1, 1, 1, 18, 3, _KeyArray.STEP));
-	}
-
-	public void actionPerformed(ActionEvent e) {
-		int x;
-		for (x = 0; x < _keys.length; ++x) {
-			if (e.getSource() == _buttons[x]) {
-				if (_keys[x].code < 0) {
-					System.out.println(_keys[x].code);
-				} else {
-					int h = _keys[x].code >> 4;
-					int l = _keys[x].code & 0x0f;
-					System.out.format("%02d %02d\n", h, l);
-				}
-				break;
-			}
-		}
+			new _KeyArray("icons/step.gif",	_KeyArray.green1, 1, 1, 0, 3, _KeyArray.STEP));
+		_col = 0;
+		_row += 4;
 	}
 }
