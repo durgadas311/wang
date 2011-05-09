e358cf44a56d1d14ae03d1f6ef52452bb077f45c
drmiller <drmiller>
2011-05-09 01:14:18 +0000

    add ROM space

--- w600-sim/w600_sys.h
+++ w600-sim/w600_sys.h
@@ -1,7 +1,7 @@
 #ifndef __w600_sys_h__
 #define __w600_sys_h__
 
-// $Id: w600_sys.h,v 1.7 2011/05/08 02:01:00 drmiller Exp $
+// $Id: w600_sys.h,v 1.8 2011/05/09 01:14:16 drmiller Exp $
 
 #include "w600_ucode.h"
 #include "w600_cpu.h"
@@ -16,6 +16,7 @@ typedef struct w600_sys_s {
 	w600_cpu_t cpu;
 	uint64_t ucode[2048];
 	uint8_t ram[2048]; // 4096x4
+	uint8_t rom[2048]; // 4096x4
 	void (*fault)(struct w600_sys_s *sys, const char *str);
 	int (*intr)(struct w600_sys_s *sys, int sig);	// return != 0 if signal not handled
 	void (*display)(struct w600_sys_s *sys, int on);
