8349c4e5893b02a6527eb58c23beddc5f72ffc30
drmiller <drmiller>
2011-05-09 10:12:20 +0000

    forgotten changes for rom/ram pre-load

--- w600-sim/w600-sim.c
+++ w600-sim/w600-sim.c
@@ -1,4 +1,4 @@
-// $Id: w600-sim.c,v 1.5 2011/05/07 16:51:02 drmiller Exp $
+// $Id: w600-sim.c,v 1.6 2011/05/09 10:12:20 drmiller Exp $
 
 #include <stdio.h>
 #include <unistd.h>
@@ -26,6 +26,25 @@ void sig_intr(int sig) {
 	exit(1);
 }
 
+static char *argv0 = NULL;
+
+static void usage() {
+	fprintf(stderr, "Usage: "
+		"%s [options]\n"
+		"Options:\n"
+		"\t-g\tDisable GUI"
+		"\t-i\tInteractive mode enable\n"
+		"\t-m file\tLoad initial contents of RAM. 2048 bytes, Lo nibble in [0]\n"
+		"\t-r file\tLoad initial contents of ROM. 2048 bytes, Lo nibble in [0]\n"
+		"\t\tNOTE: RAM/ROM contents are reverse order for program steps.\n"
+		"\t\tROM addr FFF is program step 0000 (RAM F6F is step 0000)\n"
+		"\t-t file\tTurn on TRACE to file, '-' for stderr (huge file alert!)\n"
+		"\t-p file\tUse microcode image in 'file', default 'wang600.rom'\n"
+		"\t-l addr\tLoad microcode into 'addr', default 000\n"
+		"\t-e addr\tStart running at 'addr', default 000\n"
+		,argv0);
+}
+
 int main(int argc, char **argv) {
 	sys_init(&sys);
 	int x;
@@ -33,11 +52,14 @@ int main(int argc, char **argv) {
 	char *pgm = NULL;
 	int interact = 0;
 	int sys_ops = SYS_START_GUI;
+	char *ram = NULL;
+	char *rom = NULL;
 
 	extern char *optarg;
 	extern int optind, opterr, optopt;
 
-	while ((x = getopt(argc, argv, "e:gil:p:t:")) != EOF) {
+	argv0 = argv[0];
+	while ((x = getopt(argc, argv, "e:gil:m:p:r:t:")) != EOF) {
 		switch(x) {
 		case 'e':
 			entry = strtoul(optarg, NULL, 0);
@@ -45,14 +67,20 @@ int main(int argc, char **argv) {
 		case 'g':
 			sys_ops &= ~SYS_START_GUI;
 			break;
-		case 'l':
-			load = strtoul(optarg, NULL, 0);
-			break;
 		case 'i':
 			interact = 1;
 			break;
+		case 'l':
+			load = strtoul(optarg, NULL, 0);
+			break;
+		case 'm':
+			ram = optarg;
+			break;
 		case 'p':
 			pgm = optarg;
+			break;
+		case 'r':
+			rom = optarg;
 			break;
 #ifdef TRACE
 		case 't':
@@ -69,9 +97,7 @@ int main(int argc, char **argv) {
 			break;
 #endif // TRACE
 		default:
-			fprintf(stderr, "Usage: "
-					"%s [-t file] [-i] [-p program [-l load-addr] [-e entry]]\n",
-					argv[0]);
+			usage();
 			exit(1);
 			break;
 		}
@@ -91,6 +117,12 @@ int main(int argc, char **argv) {
 
 	if (pgm) {
 		sys_loadpgm(&sys, pgm, load, entry);
+	}
+	if (ram) {
+		sys_loadram(&sys, ram);
+	}
+	if (rom) {
+		sys_loadram(&sys, rom);
 	}
 
 	sys.cmd = (interact ? 1 : 0);
