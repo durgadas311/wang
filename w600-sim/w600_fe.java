177403b4778beff2147d80a33ed1be24f877756a
drmiller <drmiller>
2011-05-16 16:26:01 +0000

    get ttf from jar

--- w600-sim/w600_fe.java
+++ w600-sim/w600_fe.java
@@ -7,7 +7,7 @@ import javax.swing.border.*;
 import java.io.*;
 
 class _Key {
-	final String ident = "$Id: w600_fe.java,v 1.30 2011/05/16 15:06:05 drmiller Exp $";
+	final String ident = "$Id: w600_fe.java,v 1.31 2011/05/16 16:26:01 drmiller Exp $";
 
 	static final Color orange1 = new Color(255, 210, 180, 255);
 	static final Color blue1 = new Color(190, 230, 255, 255);
@@ -119,7 +119,7 @@ class FEexit extends Thread {
 }
 
 public class w600_fe {
-	final String ident = "$Id: w600_fe.java,v 1.30 2011/05/16 15:06:05 drmiller Exp $";
+	final String ident = "$Id: w600_fe.java,v 1.31 2011/05/16 16:26:01 drmiller Exp $";
 
 	public static void main(String[] args) {
 		java.io.OutputStream fout = null;
@@ -165,7 +165,7 @@ public class w600_fe {
 }
 
 class Wang600_ProgErr extends JComponent {
-	final String ident = "$Id: w600_fe.java,v 1.30 2011/05/16 15:06:05 drmiller Exp $";
+	final String ident = "$Id: w600_fe.java,v 1.31 2011/05/16 16:26:01 drmiller Exp $";
 	static final long serialVersionUID = 311457692038L;
 
 	GridBagLayout gridbag = new GridBagLayout();
@@ -250,7 +250,7 @@ if (n == 1) {
 class Wang600_SimInput
 		implements Runnable
 {
-	final String ident = "$Id: w600_fe.java,v 1.30 2011/05/16 15:06:05 drmiller Exp $";
+	final String ident = "$Id: w600_fe.java,v 1.31 2011/05/16 16:26:01 drmiller Exp $";
 	Wang600_Display _dsp;
 	Wang600_Printer _prt;
 	Wang600_Tape _tape;
@@ -303,7 +303,7 @@ class Wang600_SimInput
 
 class Wang600_Printer
 {
-	final String ident = "$Id: w600_fe.java,v 1.30 2011/05/16 15:06:05 drmiller Exp $";
+	final String ident = "$Id: w600_fe.java,v 1.31 2011/05/16 16:26:01 drmiller Exp $";
 	final int PR_NUM_COL = 20;
 	final int PR_XCOL_WID = 3;
 	final int PR_XCOL_STRT = 15;
@@ -387,7 +387,7 @@ class Wang600_Printer
 
 class Wang600_Tape
 {
-	final String ident = "$Id: w600_fe.java,v 1.30 2011/05/16 15:06:05 drmiller Exp $";
+	final String ident = "$Id: w600_fe.java,v 1.31 2011/05/16 16:26:01 drmiller Exp $";
 	String _tape;
 	java.io.FileOutputStream _fo = null;
 	java.io.FileInputStream _fi = null;
@@ -484,7 +484,7 @@ class Wang600_Tape
 
 class Wang600_CN24
 {
-	final String ident = "$Id: w600_fe.java,v 1.30 2011/05/16 15:06:05 drmiller Exp $";
+	final String ident = "$Id: w600_fe.java,v 1.31 2011/05/16 16:26:01 drmiller Exp $";
 	public void do_cn24(byte[] b) {
 		if (b[1] == 0) return;
 	}
@@ -493,7 +493,7 @@ class Wang600_CN24
 class Wang600_Display extends JComponent
 		implements ActionListener
 {
-	final String ident = "$Id: w600_fe.java,v 1.30 2011/05/16 15:06:05 drmiller Exp $";
+	final String ident = "$Id: w600_fe.java,v 1.31 2011/05/16 16:26:01 drmiller Exp $";
 	static final long serialVersionUID = 311457692037L;
 	final byte[] sign_chr = new byte[]{'+','-','+','-','+','-','+','-','+','-','+','-','+','-','+',' '};
 	final byte[] disp_chr = new byte[]{'0','1','2','3','4','5','6','7','8','9','.','>','u','<','t',' '};
@@ -560,26 +560,26 @@ class Wang600_Display extends JComponent
 		disp.setBackground(_Key.empty);
 		disp.setOpaque(true);
 		Font font = null;
-//		java.io.InputStream ttf = null;
-//		ttf = Wang600_Display.class.getResourceAsStream(
-//							"fonts/Wang600Display.ttf");
-//		if (ttf != null) {
-//			try {
-//				font = Font.createFont(Font.TRUETYPE_FONT, ttf);
-//			} catch (FontFormatException ee) {
-//			} catch (IOException ee) {
-//			}
-//			font = font.deriveFont(28f);
-//		}
-//		if (font == null) {
-//			System.err.println("Missing font \"Wang600Display.ttf\", using default");
-//			font = new Font("Monospaced", Font.PLAIN, 40);
-//		}
-		font = new Font("Wang600Display", Font.PLAIN, 28);
-		if (!font.getFontName().equals("Wang600Display")) {
+		java.io.InputStream ttf = null;
+		ttf = Wang600_Display.class.getResourceAsStream(
+							"fonts/Wang600Display.ttf");
+		if (ttf != null) {
+			try {
+				font = Font.createFont(Font.TRUETYPE_FONT, ttf);
+			} catch (FontFormatException ee) {
+			} catch (IOException ee) {
+			}
+			font = font.deriveFont(28f);
+		}
+		if (font == null) {
 			System.err.println("Missing font \"Wang600Display.ttf\", using default");
 			font = new Font("Monospaced", Font.PLAIN, 40);
 		}
+//		font = new Font("Wang600Display", Font.PLAIN, 28);
+//		if (!font.getFontName().equals("Wang600Display")) {
+//			System.err.println("Missing font \"Wang600Display.ttf\", using default");
+//			font = new Font("Monospaced", Font.PLAIN, 40);
+//		}
 		disp.setPreferredSize(new Dimension(475, 75));
 		disp.setFont(font);
 
@@ -661,7 +661,7 @@ class Wang600_Display extends JComponent
 class Wang600_Keyboard extends JComponent
 	implements ActionListener, KeyListener
 {
-	final String ident = "$Id: w600_fe.java,v 1.30 2011/05/16 15:06:05 drmiller Exp $";
+	final String ident = "$Id: w600_fe.java,v 1.31 2011/05/16 16:26:01 drmiller Exp $";
 	static final long serialVersionUID = 31145769203L;
 	static final int num_kbds = 3;
 
@@ -912,7 +912,7 @@ class Wang600_Keyboard extends JComponent
 
 class Wang600_Keyboards extends JComponent
 {
-	final String ident = "$Id: w600_fe.java,v 1.30 2011/05/16 15:06:05 drmiller Exp $";
+	final String ident = "$Id: w600_fe.java,v 1.31 2011/05/16 16:26:01 drmiller Exp $";
 	static final long serialVersionUID = 311457692032L;
 	public Wang600_Keyboards() { }
 
@@ -1026,7 +1026,7 @@ class Wang600_Keyboards extends JComponent
 
 class Wang600_Keyboard_main extends Wang600_Keyboards
 {
-	final String ident = "$Id: w600_fe.java,v 1.30 2011/05/16 15:06:05 drmiller Exp $";
+	final String ident = "$Id: w600_fe.java,v 1.31 2011/05/16 16:26:01 drmiller Exp $";
 	static final long serialVersionUID = 311457692031L;
 	static final int num_keys = 54;
 
@@ -1225,7 +1225,7 @@ class Wang600_Keyboard_main extends Wang600_Keyboards
 
 class Wang600_Keyboard_meta extends Wang600_Keyboards
 {
-	final String ident = "$Id: w600_fe.java,v 1.30 2011/05/16 15:06:05 drmiller Exp $";
+	final String ident = "$Id: w600_fe.java,v 1.31 2011/05/16 16:26:01 drmiller Exp $";
 	static final long serialVersionUID = 311457692032L;
 	static final int num_keys = 16;
 
@@ -1316,7 +1316,7 @@ class Wang600_Keyboard_meta extends Wang600_Keyboards
 
 class Wang600_Keyboard_stick extends Wang600_Keyboards
 {
-	final String ident = "$Id: w600_fe.java,v 1.30 2011/05/16 15:06:05 drmiller Exp $";
+	final String ident = "$Id: w600_fe.java,v 1.31 2011/05/16 16:26:01 drmiller Exp $";
 	static final long serialVersionUID = 311457692033L;
 	static final int num_keys = 18;
 
