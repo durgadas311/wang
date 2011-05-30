86f0db3aab9feda1096f5431b70c49022d79467d
drmiller <drmiller>
2011-05-30 20:20:17 +0000

    switch to using printdialog

--- w600-sim/w600_fe.java
+++ w600-sim/w600_fe.java
@@ -1,5 +1,5 @@
 // Copyright (c) 2011 Douglas Miller
-// $Id: w600_fe.java,v 1.84 2011/05/30 15:44:42 drmiller Exp $
+// $Id: w600_fe.java,v 1.85 2011/05/30 20:20:17 drmiller Exp $
 
 import java.awt.*;
 import java.awt.event.*;
@@ -8,13 +8,11 @@ import javax.swing.border.*;
 import java.io.*;
 
 import java.awt.print.*;
-import javax.print.*;
 import javax.print.attribute.*;
 import javax.print.attribute.standard.*;
 
-
 class _Key {
-	final String ident = "$Id: w600_fe.java,v 1.84 2011/05/30 15:44:42 drmiller Exp $";
+	final String ident = "$Id: w600_fe.java,v 1.85 2011/05/30 20:20:17 drmiller Exp $";
 
 	static final Color orange1 = new Color(255, 210, 180, 255);
 	static final Color blue1 = new Color(190, 230, 255, 255);
@@ -144,7 +142,7 @@ class FEexit extends Thread {
 
 public class w600_fe
 {
-	final String ident = "$Id: w600_fe.java,v 1.84 2011/05/30 15:44:42 drmiller Exp $";
+	final String ident = "$Id: w600_fe.java,v 1.85 2011/05/30 20:20:17 drmiller Exp $";
 
 	public static File _dir = new File(System.getProperty("user.home") + "/Wang600Files");
 
@@ -279,7 +277,7 @@ public class w600_fe
 }
 
 class Wang600_ProgErr extends JComponent {
-	final String ident = "$Id: w600_fe.java,v 1.84 2011/05/30 15:44:42 drmiller Exp $";
+	final String ident = "$Id: w600_fe.java,v 1.85 2011/05/30 20:20:17 drmiller Exp $";
 	static final long serialVersionUID = 311457692038L;
 
 	GridBagLayout gridbag = new GridBagLayout();
@@ -361,7 +359,7 @@ class Wang600_SimError
 class Wang600_SimInput
 		implements Runnable, WindowListener, ActionListener
 {
-	final String ident = "$Id: w600_fe.java,v 1.84 2011/05/30 15:44:42 drmiller Exp $";
+	final String ident = "$Id: w600_fe.java,v 1.85 2011/05/30 20:20:17 drmiller Exp $";
 	Wang600_Display _dsp;
 	Wang600_Printer _prt;
 	Wang600_Tape _tape;
@@ -466,7 +464,7 @@ class Wang600_SimInput
 class Wang600_Printer
 	implements ActionListener, ComponentListener
 {
-	final String ident = "$Id: w600_fe.java,v 1.84 2011/05/30 15:44:42 drmiller Exp $";
+	final String ident = "$Id: w600_fe.java,v 1.85 2011/05/30 20:20:17 drmiller Exp $";
 	final int PR_NUM_COL = 20;
 	final int PR_XCOL_WID = 3;
 	final int PR_XCOL_STRT = 15;
@@ -600,23 +598,19 @@ class Wang600_Printer
 			return;
 		}
 		if (m.getMnemonic() == KeyEvent.VK_P) {
-			DocFlavor flavor = DocFlavor.SERVICE_FORMATTED.PRINTABLE;
 			PrintRequestAttributeSet aset = new HashPrintRequestAttributeSet();
 			aset.add(OrientationRequested.LANDSCAPE);
 			aset.add(new javax.print.attribute.standard.MediaPrintableArea(
 				(float)0.25, (float)0.25, (float)8.0, (float)10.5, MediaPrintableArea.INCH));
-			PrintService[] services =
-				PrintServiceLookup.lookupPrintServices(flavor, aset);
-			if (services.length > 0) {
-				DocPrintJob pj = services[0].createPrintJob();
+			PrinterJob pj = PrinterJob.getPrinterJob();
+			pj.setPrintable(_text);
+			boolean print = pj.printDialog(aset);
+			if (print) {
 				try {
-					Doc doc = new SimpleDoc(_text, flavor, null);
-					pj.print(doc, aset);
-				} catch (PrintException ee) { 
-System.out.println("print failed");
+					pj.print(aset);
+				} catch (PrinterException ee) { 
+					System.out.println("print failed");
 				}
-} else {
-System.out.println("no selected printer!");
 			}
 			return;
 		}
@@ -784,7 +778,7 @@ System.out.println("no selected printer!");
 
 class Wang600_Tape extends JComponent
 {
-	final String ident = "$Id: w600_fe.java,v 1.84 2011/05/30 15:44:42 drmiller Exp $";
+	final String ident = "$Id: w600_fe.java,v 1.85 2011/05/30 20:20:17 drmiller Exp $";
 	static final long serialVersionUID = 311457692039L;
 	java.io.RandomAccessFile _tf;
 	java.io.OutputStream _fout;
@@ -1383,7 +1377,7 @@ class SuffFileChooser extends JFileChooser {
 class Wang600_Model611
 	implements ActionListener, ComponentListener
 {
-	final String ident = "$Id: w600_fe.java,v 1.84 2011/05/30 15:44:42 drmiller Exp $";
+	final String ident = "$Id: w600_fe.java,v 1.85 2011/05/30 20:20:17 drmiller Exp $";
 	private byte[] cn24_xlate;
 	private String[] cn24_spcl;
 
@@ -1653,24 +1647,19 @@ class Wang600_Model611
 			return;
 		}
 		if (m.getMnemonic() == KeyEvent.VK_P) {
-			DocFlavor flavor = DocFlavor.SERVICE_FORMATTED.PRINTABLE;
 			PrintRequestAttributeSet aset = new HashPrintRequestAttributeSet();
 			aset.add(OrientationRequested.LANDSCAPE);
 			aset.add(new javax.print.attribute.standard.MediaPrintableArea(
 				(float)0.75, (float)0.5, (float)7.0, (float)10.0, MediaPrintableArea.INCH));
-			PrintService[] services =
-				PrintServiceLookup.lookupPrintServices(flavor, aset);
-			if (services.length > 0) {
-				DocPrintJob pj = services[0].createPrintJob();
+			PrinterJob pj = PrinterJob.getPrinterJob();
+			pj.setPrintable(_text);
+			boolean print = pj.printDialog(aset);
+			if (print) {
 				try {
-					Doc doc = new SimpleDoc(_text, flavor, null);
-					pj.print(doc, aset);
-				} catch (PrintException ee) { 
-System.out.println("print failed");
+					pj.print(aset);
+				} catch (PrinterException ee) { 
+					System.out.println("print failed");
 				}
-} else {
-// Why does this sometimes fail? but work just by retry?
-System.out.println("no selected printer!");
 			}
 			return;
 		}
@@ -1969,7 +1958,7 @@ System.out.println("no selected printer!");
 class Wang600_Display extends JComponent
 		implements ActionListener
 {
-	final String ident = "$Id: w600_fe.java,v 1.84 2011/05/30 15:44:42 drmiller Exp $";
+	final String ident = "$Id: w600_fe.java,v 1.85 2011/05/30 20:20:17 drmiller Exp $";
 	static final long serialVersionUID = 311457692037L;
 	final byte[] sign_chr = new byte[]{'+','-','+','-','+','-','+','-','+','-','+','-','+','-','+',' '};
 	final byte[] disp_chr = new byte[]{'0','1','2','3','4','5','6','7','8','9','.','>','u','<','t',' '};
@@ -2110,7 +2099,7 @@ class Wang600_Display extends JComponent
 class Wang600_Keyboard extends JComponent
 	implements ActionListener, KeyListener, WindowListener, ComponentListener
 {
-	final String ident = "$Id: w600_fe.java,v 1.84 2011/05/30 15:44:42 drmiller Exp $";
+	final String ident = "$Id: w600_fe.java,v 1.85 2011/05/30 20:20:17 drmiller Exp $";
 	static final long serialVersionUID = 31145769203L;
 	static final int num_kbds = 3;
 
@@ -2581,7 +2570,7 @@ class Wang600_Keyboard extends JComponent
 
 class Wang600_Keyboards extends JComponent
 {
-	final String ident = "$Id: w600_fe.java,v 1.84 2011/05/30 15:44:42 drmiller Exp $";
+	final String ident = "$Id: w600_fe.java,v 1.85 2011/05/30 20:20:17 drmiller Exp $";
 	static final long serialVersionUID = 311457692034L;
 	public Wang600_Keyboards() { }
 
@@ -2746,7 +2735,7 @@ class Wang600_Keyboards extends JComponent
 
 class Wang600_Keyboard_main extends Wang600_Keyboards
 {
-	final String ident = "$Id: w600_fe.java,v 1.84 2011/05/30 15:44:42 drmiller Exp $";
+	final String ident = "$Id: w600_fe.java,v 1.85 2011/05/30 20:20:17 drmiller Exp $";
 	static final long serialVersionUID = 311457692031L;
 	static final int num_keys = 54;
 
@@ -2962,7 +2951,7 @@ class Wang600_Keyboard_main extends Wang600_Keyboards
 
 class Wang600_Keyboard_meta extends Wang600_Keyboards
 {
-	final String ident = "$Id: w600_fe.java,v 1.84 2011/05/30 15:44:42 drmiller Exp $";
+	final String ident = "$Id: w600_fe.java,v 1.85 2011/05/30 20:20:17 drmiller Exp $";
 	static final long serialVersionUID = 311457692032L;
 	static final int num_keys = 16;
 
@@ -3053,7 +3042,7 @@ class Wang600_Keyboard_meta extends Wang600_Keyboards
 
 class Wang600_Keyboard_stick extends Wang600_Keyboards
 {
-	final String ident = "$Id: w600_fe.java,v 1.84 2011/05/30 15:44:42 drmiller Exp $";
+	final String ident = "$Id: w600_fe.java,v 1.85 2011/05/30 20:20:17 drmiller Exp $";
 	static final long serialVersionUID = 311457692033L;
 	static final int num_keys = 22;
 
