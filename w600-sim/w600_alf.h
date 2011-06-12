dfa60be89d21663f123ce2dd9aa6a7e06070fd75
drmiller <drmiller>
2011-06-12 15:34:55 +0000

    fixups

--- w600-sim/w600_alf.h
+++ w600-sim/w600_alf.h
@@ -1,13 +1,13 @@
 #ifndef __w600_alf_h__
 #define __w600_alf_h__
 
-#ident "$Id: w600_alf.h,v 1.1 2011/06/12 15:14:33 drmiller Exp $ Copyright (c) 2008-2011 Alf Urban"
+#ident "$Id: w600_alf.h,v 1.2 2011/06/12 15:34:55 drmiller Exp $ Copyright (c) 2008-2011 Alf Urban"
 
-struct
+typedef struct
 {
 	char magic[8], date[9], author[15], title[24];
 	short order, block, bytes, verify;
-}
+} w600_alf_t;
 
 #define ALF_MAGIC	"WANG600\0"
 
