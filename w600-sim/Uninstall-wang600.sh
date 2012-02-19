27a739912f9f40ce65d62310c2ed80446c7325ae
drmiller <drmiller>
2012-02-19 02:41:35 +0000

    fix RO uninstall problem

--- w600-sim/Uninstall-wang600.sh
+++ w600-sim/Uninstall-wang600.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
-# $Id: Uninstall-wang600.sh,v 1.1 2012/02/19 01:51:10 drmiller Exp $
+# $Id: Uninstall-wang600.sh,v 1.2 2012/02/19 02:41:35 drmiller Exp $
 
 # paths might contain blanks!
 cd "${HOME}"
 
 if [[ -d "${HOME}/Desktop/Wang600.app" ]]; then
 	echo "Removing Wang600 App..."
+	chmod -R u+w "${HOME}/Desktop/Wang600.app"
 	rm -rf "${HOME}/Desktop/Wang600.app"
 fi
