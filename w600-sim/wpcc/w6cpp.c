a975f7ebb22606935af3a370a92c13d99ae0d29f
drmiller <drmiller>
2013-02-04 15:15:12 +0000

    make data conversion routine common

--- w600-sim/wpcc/w6cpp.c
+++ w600-sim/wpcc/w6cpp.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
 
 #define EX_PLOT		'\001'
 #define EX_MOVE		'\002'
@@ -56,45 +57,16 @@ void do_enter(char *s) {
 
 void do_data(char *l, char *s) {
 	int x;
-	char buf[32], *e = NULL;
-	double d;
-	d = strtod(s, &e);
-	if (e == s || *e != '\0') {
-		fprintf(stderr, "%s: %d: Not a floating point number: \"%s\"\n",
-			__file__, __line__, s);
+	uint8_t reg[16];
+
+	extern int w6_do_data(char *s, uint8_t out[16]);
+
+	if (w6_do_data(s, reg) != 0) {
 		return;
 	}
-	sprintf(buf + 1, "%17.11e", d);
-	e = buf + 1;
-	if (*e != '-') {
-		--e;
-		*e = '+';
-	}
 	printf("\t_regdata(%s", l);
-	x = 0;
-	while (*e && x < 16) {
-		int n = 0;
-		int c = *e++;
-		if (isdigit(c)) {
-			n = c - '0';
-		} else if (c == '.') {
-			//n = 10;
-			continue;
-		} else if (c == '+') {
-			n = 0;
-		} else if (c == '-') {
-			n = 1;
-		} else if (c == 'e' || c == 'E') {
-			while (x < 13) {
-				printf(",0");
-				++x;
-			}
-			continue;
-		} else {
-			continue;
-		}
-		printf(",%d", n);
-		++x;
+	for (x = 0; x < 16; ++x) {
+		printf(",%d", reg[x]);
 	}
 	printf(");\n");
 }
---
+++ w600-sim/wpcc/w6cpp_data.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+
+extern int __line__;
+extern char *__file__;
+
+int w6_do_data(char *s, uint8_t out[16]) {
+	int x;
+	char buf[32], *e = NULL;
+	double d;
+	d = strtod(s, &e);
+	if (e == s || (*e != '\0' && *e != '\n')) {
+		fprintf(stderr, "%s: %d: Not a floating point number: \"%s\"\n",
+			__file__, __line__, s);
+		return -1;
+	}
+	sprintf(buf + 1, "%17.11e", d);
+	e = buf + 1;
+	if (*e != '-') {
+		--e;
+		*e = '+';
+	}
+	x = 0;
+	while (*e && x < 16) {
+		int n = 0;
+		int c = *e++;
+		if (isdigit(c)) {
+			n = c - '0';
+		} else if (c == '.') {
+			//n = 10;
+			continue;
+		} else if (c == '+') {
+			n = 0;
+		} else if (c == '-') {
+			n = 1;
+		} else if (c == 'e' || c == 'E') {
+			while (x < 13) {
+				out[x] = 0;
+				++x;
+			}
+			continue;
+		} else {
+			continue;
+		}
+		out[x] = n;
+		++x;
+	}
+	while (x < 16) {
+		out[x] = 0;
+		++x;
+	}
+	return 0;
+}
