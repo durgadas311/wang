3e44c488875562e0967c2fe99c8178d4a55bea85
drmiller <drmiller>
2013-12-04 14:00:49 +0000

    add java converter for ascii to w1200 and remove unused files

---
+++ w1200-sim/Wang1200_TextToTape.java
@@ -0,0 +1,117 @@
+// Copyright (c) 2013 Douglas Miller
+// $Id: Wang1200_TextToTape.java,v 1.1 2013/12/04 14:00:49 drmiller Exp $ 
+
+import java.io.*;
+import java.util.Arrays;
+
+public class Wang1200_TextToTape
+{
+	private class w1200_line {
+		byte[] hdr = new byte[8];
+		byte[] line = new byte[100];
+		public void write(OutputStream out) {
+			try {
+				out.write(hdr);
+				out.write(line);
+			} catch(Exception ee) {
+			}
+		}
+		public void reset() {
+			Arrays.fill(hdr, (byte)0x00);
+			Arrays.fill(line, (byte)0xaa);
+		}
+	}
+
+	private BufferedReader _in;
+	private byte[] escapes = new byte[256];
+	private Wang_CharConverter _cnv;
+	private boolean _eod;
+	private String _name;
+	private int _line;
+	w1200_line _buf;
+
+	private int cvt_line(String inb, w1200_line buf) {
+		buf.reset();
+		char c = '\0';
+		int y = 0;
+		int x = 0;
+		byte t;
+try {
+		while (y < buf.line.length && c != '\n') {
+			_eod = false;
+			c = inb.charAt(x++);
+			boolean code = (c == '['); // CODE characters...
+			if (code) {
+				c = inb.charAt(x++);
+				if (inb.charAt(x) == '\\') {
+					// ASCII escapes...
+					++x;
+					char cx = inb.charAt(x++);
+					c = (char)escapes[Character.toLowerCase(cx)];
+					if (c == '\0') {
+						System.err.format("%s:%d: invalid escape character '%c'\n", _name, _line, cx);
+						return 1;
+					}
+				}
+				if (inb.charAt(x) != ']') {
+					System.err.format("%s:%d: invalid code string for '%c' @%d:\"%s\"\n", _name, _line, c, x, inb);
+					return 1;
+				}
+				if (c == '/') {	// EOD - strip off \n
+					if (inb.charAt(x) == '\n') ++x;
+					_eod = true;
+				}
+			}
+			t = _cnv.asciiToCodedTiltrotate((byte)c, code);
+			buf.line[y++] = t;
+			if (t == (byte)0x0a) { c = '\n'; }
+		}
+} catch(IndexOutOfBoundsException ee) {
+			// should check context... e.g. not in CODE sequence.
+			c = '\n';
+}
+		if (c != '\n') {
+			System.err.format( "%s:%d: line overflow\n", _name, _line);
+			return 1;
+		}
+		return 0;
+	}
+
+	public Wang1200_TextToTape() {
+		_buf = new w1200_line();
+		escapes['b'] = '\b';
+		escapes['n'] = '\n';
+		escapes['t'] = '\t';
+		_cnv = new Wang_CharConverter(true);
+	}
+
+	// converts stdin to stdout... stdout is binary 108-block formatted...
+	public void textToTape(String name, InputStream in, OutputStream out) {
+		_in = new BufferedReader(new InputStreamReader(in));
+		_name = name;
+
+		int e = 0;
+		_line = 0;
+		_eod = false;
+		while (e == 0) {
+			++_line;
+			try {
+				e = -1;
+				String s = _in.readLine();
+				if (s != null) {
+					e = cvt_line(s + '\n', _buf);
+				}
+			} catch(Exception ee) { }
+			if (e == 0) {
+				_buf.write(out);
+			}
+		}
+		if (!_eod) {
+			++_line;
+			e = cvt_line("[/]\n", _buf);
+			if (e == 0) {
+				_buf.write(out);
+			}
+		}
+	}
+}
--- w1200-sim/txt2wng.java
+++ w1200-sim/txt2wng.java
@@ -1,112 +1,47 @@
 // Copyright (c) 2013 Douglas Miller
-// $Id: txt2wng.java,v 1.1 2013/12/03 23:05:36 drmiller Exp $ 
+// $Id: txt2wng.java,v 1.2 2013/12/04 14:00:49 drmiller Exp $ 
 
 import java.io.*;
-import java.util.Arrays;
-
-	class w1200_line {
-		byte[] hdr = new byte[8];
-		byte[] line = new byte[100];
-		public void write(OutputStream out) {
-			try {
-				out.write(hdr);
-				out.write(line);
-			} catch(Exception ee) {
-			}
-		}
-		public void reset() {
-			Arrays.fill(hdr, (byte)0x00);
-			Arrays.fill(line, (byte)0xaa);
-		}
-	}
 
 public class txt2wng
 {
-	static private BufferedReader _in;
-
-	static private byte[] escapes = new byte[256];
-	static private Wang_CharConverter _cnv;
-
-	static private boolean _eod;
-	static private int _line;
-
-	static private int cvt_line(String inb, w1200_line buf) {
-		buf.reset();
-		char c = '\0';
-		int y = 0;
-		int x = 0;
-		byte t;
-try {
-		while (y < buf.line.length) {
-			_eod = false;
-			c = inb.charAt(x++);
-			boolean code = (c == '['); // CODE characters...
-			if (code) {
-				c = inb.charAt(x++);
-				if (inb.charAt(x) == '\\') {
-					// ASCII escapes...
-					++x;
-					char cx = inb.charAt(x++);
-					c = (char)escapes[Character.toLowerCase(cx)];
-					if (c == '\0') {
-						System.err.format("%d: invalid escape character '%c'\n", _line, cx);
-						return 1;
-					}
-				}
-				if (inb.charAt(x) != ']') {
-					System.err.format("%d: invalid code string for '%c' @%d:\"%s\"\n", _line, c, x, inb);
-					return 1;
-				}
-				if (c == '/') {	// EOD - strip off \n
-					if (inb.charAt(x) == '\n') ++x;
-					_eod = true;
-					c = '\n';
-				}
-			}
-			t = _cnv.asciiToCodedTiltrotate((byte)c, code);
-			buf.line[y++] = t;
-			if (c == '\n' || t == (byte)0x0a) break;
-		}
-} catch(IndexOutOfBoundsException ee) {
-}
-		if (c != '\n') {
-			System.err.format( "%d: line overflow\n", _line);
-			return 1;
-		}
-		return 0;
-	}
-
 	// converts stdin to stdout... stdout is binary 108-block formatted...
 	public static void main(String[] args) {
-		w1200_line buf = new w1200_line();
-		_in = new BufferedReader(new InputStreamReader(System.in));
-		escapes['b'] = '\b';
-		escapes['n'] = '\n';
-		escapes['t'] = '\t';
-		_cnv = new Wang_CharConverter(true);
-
-		int e = 0;
-
-		_line = 0;
-
-		while (e == 0) {
-			++_line;
-			try {
-				String s = _in.readLine();
-				e = cvt_line(s, buf);
-			} catch(Exception ee) {
-				e = -1;
+		Wang1200_TextToTape cnv = new Wang1200_TextToTape();
+		if (args.length > 0) {
+			for (int x = 0; x < args.length; ++x) {
+				String f = args[x];
+				int ix = f.lastIndexOf(".txt");
+				if (ix > 0) {
+					f = f.substring(0, ix);
+				}
+				f += ".wpt";
+				FileInputStream fin = null;
+				FileOutputStream fout = null;
+				try {
+					fin = new FileInputStream(args[x]);
+				} catch(Exception ee) {
+					System.err.println(args[x] + ": " + ee.getMessage());
+				}
+				try {
+					fout = new FileOutputStream(f);
+				} catch(Exception ee) {
+					System.err.println(f + ": " + ee.getMessage());
+				}
+				if (fin != null && fout != null) {
+					cnv.textToTape(args[x], fin, fout);
+					try {
+						fin.close();
+					} catch(Exception ee) { }
+					try {
+						fout.close();
+					} catch(Exception ee) {
+						System.err.println(f + ": " + ee.getMessage());
+					}
+				}
 			}
-			if (e == 0) {
-				buf.write(System.out);
-			}
-		}
-		if (!_eod) {
-			++_line;
-			e = cvt_line("[/]", buf);
-			if (e == 0) {
-				buf.write(System.out);
-			}
+		} else {
+			cnv.textToTape("stdin", System.in, System.out);
 		}
 	}
 }
