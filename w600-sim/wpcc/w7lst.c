bbdda3d4c31f1b9180c22ab5f1498953d7368d46
drmiller <drmiller>
2013-12-24 16:00:45 +0000

    enhance Wang700 list program

--- w600-sim/wpcc/w7lst.c
+++ w600-sim/wpcc/w7lst.c
@@ -50,20 +50,111 @@ char *op[16][16] = {
 	"RE RES",
 	"CLEAR X"
 },
+// WRITE_ALPHA prefixed codes...
+[12] = {
+	"\u00D7 1e-0", "\u00D7 1e-1", "\u00D7 1e-2", "\u00D7 1e-3", "\u00D7 1e-4",
+	"\u00D7 1e-5", "\u00D7 1e-6", "\u00D7 1e-7", "\u00D7 1e-8", "\u00D7 1e-9",
+	"SK IF Y+",
+	"SK IF Y=0",
+	NULL,
+	"END ALPHA",
+	NULL,
+	NULL
+},
+[13] = {
+	NULL, NULL, NULL, NULL, NULL,
+	NULL, NULL, NULL, NULL, NULL,
+	"SK IF Y-",
+	"SK IF Y\u22600",
+	NULL,
+	NULL,
+	"180/\u03C0",
+	"\u03C0/180"
+},
+[14] = {
+	NULL, NULL, NULL, NULL, NULL,
+	NULL, NULL, NULL, NULL, NULL,
+	"SK IF X+",
+	"SK IF X=0",
+	NULL,
+	NULL,
+	NULL,
+	"PAUSE"
+},
+[15] = {
+	"\u00D7 1e0", "\u00D7 1e1", "\u00D7 1e2", "\u00D7 1e3", "\u00D7 1e4",
+	"\u00D7 1e5", "\u00D7 1e6", "\u00D7 1e7", "\u00D7 1e8", "\u00D7 1e9",
+	"SK IF X-",
+	"SK IF X\u22600",
+	NULL,
+	NULL,
+	NULL,
+	NULL
+},
 };
 
 static char *decode(uint8_t a, uint8_t b) {
 	static char buf[128];
+	static int reg = 0;
 
 	buf[0] = '\0';
 	// detect DIRECT codes and list register number differently?
-	if (a < 4) {
+	if (reg) {
+		switch(reg - 1) {
+		case 12:	// WRITE_ALPHA
+			if (a < 4) {
+				sprintf(buf, "PRINT %d-%d", a, b);
+			} else if (a < 8) {
+				if (op[a + 8][b] == NULL) {
+					sprintf(buf, "?? %02d%02d", a, b);
+				} else {
+					sprintf(buf, "%s", op[a + 8][b]);
+				}
+				reg = 0;
+			} else if (a < 12) {
+				sprintf(buf, "PLOT %d-%d", a & 7, b);
+			} else {
+				sprintf(buf, "?? %02d%02d", a, b);
+				reg = 0;
+			}
+			break;
+		case 7:		// SEARCH
+			sprintf(buf, "SEARCH %02d%02d", a, b);
+			reg = 0;
+			break;
+		case 8:		// MARK
+			sprintf(buf, "MARK %02d%02d", a, b);
+			reg = 0;
+			break;
+		case 9:		// GRP1
+			sprintf(buf, "GRP1 %02d%02d", a, b);
+			reg = 0;
+			break;
+		case 10:	// GRP2
+			sprintf(buf, "GRP2 %02d%02d", a, b);
+			reg = 0;
+			break;
+		case 11:	// WRITE
+			sprintf(buf, "WRITE %02d%02d", a, b);
+			reg = 0;
+			break;
+		default:
+			sprintf(buf, "REG %02d%02d", a, b);
+			reg = 0;
+			break;
+		}
+
+
+	} else if (a < 4) {
 		sprintf(buf, "SR %02d%02d", a, b);
 	} else if (a == 12) {
 		if (b < 7 || b > 13) {
 			sprintf(buf, "%s+100", op[4][b]);
 		}
 	} else if (a < 8) {
+		if (a == 4) {
+			reg = b + 1;
+		}
 		sprintf(buf, "%s", op[a][b]);
 	}
 	return buf;
