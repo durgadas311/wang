04f20d2faffb5865ef6d377ae49754f7fa46e8b1
drmiller <drmiller>
2014-01-25 23:38:08 +0000

    .

--- w700-sim/Release.java
+++ w700-sim/Release.java
@@ -1,3 +1,3 @@
 class Release {
-	static final String ident = "$Revision: 1.9 $ $Date: 2014/01/25 23:27:53 $ $State: Exp $";
+	static final String ident = "$Revision: 1.10 $ $Date: 2014/01/25 23:38:08 $ $State: Rel $";
 }
