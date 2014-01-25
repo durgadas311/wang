c9b82d8c40778aef9fa42c0bf784d2e2a192e23a
drmiller <drmiller>
2014-01-25 22:34:59 +0000

    .

--- w700-sim/Revision.java
+++ w700-sim/Revision.java
@@ -1,3 +1,3 @@
 class Revision {
-	static final String label = "$Id: Revision.java,v 1.1 2014/01/25 22:27:54 drmiller Exp $";
+	static final String label = "$Id: Revision.java,v 1.2 2014/01/25 22:34:59 drmiller Rel $";
 }
