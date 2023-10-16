1cfa01649cedf904747e3ca3404da1634772c280
Douglas Miller <durgadas311@gmail.com>
2023-10-16 10:23:31 -0500

    add program to find orphans in microcode

---
+++ w700-sim/ucorphans.c
@@ -0,0 +1,83 @@
+// Copyright (c) 2023, Douglas Miller <durgadas311@gmail.com>
+
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+
+/* look for orphaned code in ROM720C */
+
+#include "w700_ucode.h"
+
+extern int loaducode_txt(int fd, uint64_t *m, int len);
+extern void diwang(char *buf, uint64_t *v);
+
+char calls[2 * 1024] = {0};
+char buf[1024];
+
+int main(int argc, char **argv) {
+	int rc;
+	uint64_t *ucode;
+	w700_ucode_t *u;
+	size_t ucodez = 2 * 1024 * sizeof(*ucode);
+	int adr;
+	int x;
+
+	if (argc != 2) {
+		fprintf(stderr, "Usage: %s ucode-file\n", argv[0]);
+		return 1;
+	}
+
+	int fd = open(argv[1], O_RDONLY);
+	if (fd < 0) {
+		perror(argv[1]);
+		return 1;
+	}
+	// assume text format...
+	ucode = (uint64_t *)malloc(ucodez);
+	rc = loaducode_txt(fd, ucode, ucodez);
+	// what's a good error check?
+	if (rc != ucodez) {
+		fprintf(stderr, "loaducode_txt() returned %d\n", rc);
+	}
+	close(fd);
+
+	/* these are called by hardware */
+	calls[0x000]++;
+	calls[0x001]++;
+	calls[0x002]++;
+	calls[0x003]++;
+	calls[0x004]++;
+	calls[0x005]++;
+	calls[0x006]++;
+	calls[0x007]++;
+
+	for (x = 0; x < 2 * 1024; ++x) {
+		u = (w700_ucode_t *)&ucode[x];
+		adr = u->jad << 2;
+		if (u->jl == 1) adr |= 0x001;
+		if (u->jh == 1) adr |= 0x002;
+		calls[adr]++;
+		if (u->jl > 1) calls[adr | 0x001]++;
+		if (u->jh > 1) calls[adr | 0x002]++;
+	}
+
+	for (x = 0; x < 2 * 1024; ++x) {
+		if (ucode[x] != 0UL && calls[x] == 0) {
+			u = (w700_ucode_t *)&ucode[x];
+			diwang(buf, &ucode[x]);
+			printf("%03x: [%x%x%x%x%x%x%x%x%x%x%03x%x%x] %s\n",x,
+				u->ai, u->bi, u->zo, u->aop, u->ac, u->bc, u->bd,
+				u->mop, u->kk, u->st,
+				u->jad << 2, u->jh, u->jl,
+				buf);
+		}
+	}
+
+	free(ucode);
+	return 0;
+}
