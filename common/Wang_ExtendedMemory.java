de16e0036a813fbbb890b31a2a67a37b2b3e385e
Douglas Miller <durgadas311@gmail.com>
2026-09-07 07:41:26 -0500

    add GUI to extended memory device

--- common/Wang_ExtendedMemory.java
+++ common/Wang_ExtendedMemory.java
@@ -12,11 +12,12 @@ import javax.swing.*;
 import javax.swing.text.*;
 import javax.swing.border.*;
 
-public class Wang_ExtendedMemory
+public class Wang_ExtendedMemory extends JFrame
 		implements Wang_BlockIODevice,
-			ActionListener, Runnable {
+			WindowListener, ActionListener, MouseListener, Runnable {
 	static final int ramSize = 4096; // all units are the same
-	boolean debug = false;
+	static final Color unitGone = Wang_Colors.gray;
+	static final Color unitInst = Wang_Colors.green1;
 
 	static String Model = "08";
 	static String Name = "Extended Memory";
@@ -52,6 +53,8 @@ public class Wang_ExtendedMemory
 	private static final int OPTION_CANCEL = 2;
 	String _prop;
 	JCheckBox[] _devs;
+	JPanel[] _units;
+	LED[] _leds;
 
 	boolean devEna;
 	boolean gkbd;
@@ -67,8 +70,14 @@ public class Wang_ExtendedMemory
 	int curUnit;
 	byte[][] _ram;
 	int _ramMask;
+	boolean visib;
 
 	public Wang_ExtendedMemory(String pfx) {
+		super("Wang " + s_getName());
+		getContentPane().setName("Wang " + s_getName());
+		setResizable(false);
+		addWindowListener(this);
+
 		_pfx = pfx;
 		giChr = new java.util.concurrent.LinkedBlockingDeque<Integer>();
 
@@ -77,23 +86,86 @@ public class Wang_ExtendedMemory
 		// GBM switches - address config dialog
 		_ram = new byte[16][];
 		_devs = new JCheckBox[16];
+		_units = new JPanel[16];
+		_leds = new LED[16];
+		curUnit = -1;
 		int n = 0;
 		for (int x = 0; x < 16; ++x) {
 			_devs[x] = new JCheckBox(String.format("00-%02d %dK", x, ramSize / 1024));
+			_leds[x] = new RoundLED(LED.Colors.NEON);
+			_units[x] = getMemoryUnit(x); // after _leds[] set
 			_prop = String.format("%sdev%02d", _pfx, x);
+			// TODO: somehow allow for none configuired? (why?)
 			if (Wang_UI.getProperties().getProperty(_prop) != null) {
-				_devs[x].setSelected(true);
-				_ram[x] = new byte[ramSize];
+				cfgUnit(x);
 				++n;
 			}
 		}
 		if (n == 0) { // always at least one, at 00
-			_devs[0].setSelected(true);
-			_ram[0] = new byte[ramSize];
+			cfgUnit(0);
 		}
-		_dia_pn = new JPanel();
+
+		JMenuBar mb = new JMenuBar();
+		//JMenu mu = new JMenu("Memory");
+		JMenuItem mi = new JMenuItem("Config Units", KeyEvent.VK_U);
+		mi.addActionListener(this);
+		mb.add(mi);
+		//mu.add(mi);
+		//mi = new JMenuItem("Save Core", KeyEvent.VK_S);
+		//mi.addActionListener(this);
+		//mu.add(mi);
+		//mi = new JMenuItem("Load Core", KeyEvent.VK_L);
+		//mi.addActionListener(this);
+		//mu.add(mi);
+		//mb.add(mu);
+		setJMenuBar(mb);
+
 		gb = new GridBagLayout();
 		gc = new GridBagConstraints();
+		gc.fill = GridBagConstraints.NONE;
+		gc.gridx = 0;
+		gc.gridy = 0;
+		gc.weightx = 1;
+		gc.weighty = 1;
+		gc.gridwidth = 1;
+		gc.gridheight = 1;
+		gc.insets.left = 0;
+		gc.insets.right = 0;
+		gc.anchor = GridBagConstraints.WEST;
+		setLayout(gb);
+		JPanel pn = new JPanel();
+		pn.setPreferredSize(new Dimension(10, 10));
+		gb.setConstraints(pn, gc);
+		add(pn);
+		++gc.gridx;
+		++gc.gridy;
+		for (int x = 0; x < 16; ++x) {
+			gc.gridx = (x >> 2) * 2 + 1;
+			gc.gridy = (x & 3) * 2 + 1;
+			gb.setConstraints(_units[x], gc);
+			add(_units[x]);
+			++gc.gridx;
+			++gc.gridy;
+			pn = new JPanel();
+			pn.setPreferredSize(new Dimension(10, 10));
+			gb.setConstraints(pn, gc);
+			add(pn);
+		}
+		setupDialog();
+		pack();
+		onOff(false);
+		setLocationByPlatform(true);
+
+		reset();
+
+		Thread t = new Thread(this);
+		t.start();
+	}
+
+	private void setupDialog() {
+		_dia_pn = new JPanel();
+		GridBagLayout gb = new GridBagLayout();
+		GridBagConstraints gc = new GridBagConstraints();
 		gc.fill = GridBagConstraints.NONE;
 		gc.gridx = 0;
 		gc.gridy = 0;
@@ -117,23 +189,74 @@ public class Wang_ExtendedMemory
 		_btns[OPTION_CANCEL] = "Cancel";
 		_prefs = new JOptionPane(_dia_pn, JOptionPane.QUESTION_MESSAGE,
 			JOptionPane.YES_NO_CANCEL_OPTION, Wang_UI.getIcon(), _btns);
-
-		reset();
-
-		Thread t = new Thread(this);
-		t.start();
 	}
 
-	private void debugDump() {
-		if (_ram[curUnit] == null) return;
-		try {
-			String f = String.format("w608core.%02", curUnit);
-			OutputStream os = new FileOutputStream(f);
-			os.write(_ram[curUnit]);
-			os.close();
-		} catch (Exception ee) {
-			ee.printStackTrace();
+	private JPanel getMemoryUnit(int x) {
+		JPanel pan = new JPanel();
+		pan.setPreferredSize(new Dimension(100, 40));
+		Border lb = BorderFactory.createBevelBorder(BevelBorder.RAISED);
+		pan.setBorder(lb);
+		pan.addMouseListener(this);
+		GridBagLayout gb = new GridBagLayout();
+		GridBagConstraints gc = new GridBagConstraints();
+		gc.fill = GridBagConstraints.NONE;
+		gc.gridx = 0;
+		gc.gridy = 0;
+		gc.weightx = 1;
+		gc.weighty = 1;
+		gc.gridwidth = 1;
+		gc.gridheight = 1;
+		gc.insets.left = 0;
+		gc.insets.right = 0;
+		gc.anchor = GridBagConstraints.WEST;
+		pan.setLayout(gb);
+		JPanel pn = new JPanel();
+		pn.setPreferredSize(new Dimension(5, 5));
+		pn.setOpaque(false);
+		gb.setConstraints(pn, gc);
+		pan.add(pn);
+		++gc.gridx;
+		gb.setConstraints(_leds[x], gc);
+		pan.add(_leds[x]);
+		++gc.gridx;
+		JLabel lab = new JLabel(String.format("00-%02d (%dK)", x, ramSize / 1024));
+		gb.setConstraints(lab, gc);
+		pan.add(lab);
+		++gc.gridx;
+		pn = new JPanel();
+		pn.setPreferredSize(new Dimension(5, 5));
+		pn.setOpaque(false);
+		gb.setConstraints(pn, gc);
+		pan.add(pn);
+		pan.setBackground(unitGone);
+		return pan;
+	}
+
+	private int getUnit(Object src) {
+		int x;
+		for (x = 0; x < 16; ++x) {
+			if (_units[x] == src) return x;
 		}
+		return -1;
+	}
+
+	private void cfgUnit(int x) {
+		//_units[x].setVisible(true);
+		_units[x].setBackground(unitInst);
+		_devs[x].setSelected(true);
+		_ram[x] = new byte[ramSize];
+	}
+
+	private void decfgUnit(int x) {
+		//_units[x].setVisible(false);
+		_units[x].setBackground(unitGone);
+		_devs[x].setSelected(false);
+		_leds[x].set(false); // just in case
+		if (curUnit == x) {
+			curUnit = -1;
+			devEna = false;
+		}
+		_ram[x] = null;
 	}
 
 	private int readRAM(int adr) {
@@ -191,13 +314,56 @@ public class Wang_ExtendedMemory
 		}
 	}
 
+	private File pickFile(String purpose) {
+		File file;
+		SuffFileChooser ch = new SuffFileChooser(purpose,
+			Wang_UI.getDir());
+		int rv = ch.showDialog(this);
+		if (rv == JFileChooser.APPROVE_OPTION) {
+			file = ch.getSelectedFile();
+		} else {
+			file = null;
+		}
+		return file;
+	}
+
+	private void saveCore(int x) {
+		String s = String.format("Save 00-%02d Core", x);
+		File core = pickFile(s);
+		if (core == null) return;
+		try {
+			OutputStream os = new FileOutputStream(core);
+			os.write(_ram[x]);
+			os.close();
+		} catch (Exception ee) {
+			Wang_UI.warning(s, ee.getMessage());
+		}
+	}
+
+	private void loadCore(int x) {
+		String s = String.format("Load 00-%02d Core", x);
+		File core = pickFile(s);
+		if (core == null) return;
+		try {
+			InputStream is = new FileInputStream(core);
+			is.read(_ram[x]);
+			is.close();
+		} catch (Exception ee) {
+			Wang_UI.warning(s, ee.getMessage());
+		}
+	}
+
 	public void actionPerformed(ActionEvent e) {
 		Object src = e.getSource();
 		if (!(src instanceof JMenuItem)) return;
 		JMenuItem mi = (JMenuItem)src;
 		int mn = mi.getMnemonic();
-		if (mn != KeyEvent.VK_D) return;
-		Dialog dlg = _prefs.createDialog(null, "Set " + getModel() + " Units");
+		if (mn == KeyEvent.VK_D) {
+			onOff(true);
+			return;
+		}
+		if (mn != KeyEvent.VK_U) return;
+		Dialog dlg = _prefs.createDialog(this, "Set " + getModel() + " Units");
 		dlg.setVisible(true);
 		Object res = _prefs.getValue();
 		if (_btns[OPTION_CANCEL].equals(res)) return;
@@ -211,11 +377,11 @@ try {
 				if (_devs[x].isSelected()) {
 					temp.setProperty(_prop, "yes");
 					if (_ram[x] == null) {
-						_ram[x] = new byte[ramSize];
+						cfgUnit(x);
 					}
 				} else {
 					temp.remove(_prop);
-					_ram[x] = null;
+					decfgUnit(x);
 				}
 			}
 			if (_btns[OPTION_SAVE].equals(res)) {
@@ -224,6 +390,32 @@ try {
 } catch (Exception ee) {}
 		}
 	}
+
+	public void windowActivated(WindowEvent e) { }
+	public void windowClosed(WindowEvent e) { }
+	public void windowIconified(WindowEvent e) { }
+	public void windowOpened(WindowEvent e) { }
+	public void windowDeiconified(WindowEvent e) { }
+	public void windowDeactivated(WindowEvent e) { }
+	public void windowClosing(WindowEvent e) {
+		onOff(false);
+	}
+
+	public void mouseClicked(MouseEvent e) {
+		int x = getUnit(e.getSource());
+		if (x < 0 || !_devs[x].isSelected()) return;
+		//int m = e.getModifiersEx();
+		int b = e.getButton();
+		if (b == MouseEvent.BUTTON1) {
+			saveCore(x);
+		} else if  (b == MouseEvent.BUTTON3) {
+			loadCore(x);
+		}
+	}
+	public void mouseEntered(MouseEvent e) { }
+	public void mouseExited(MouseEvent e) { }
+	public void mousePressed(MouseEvent e) { }
+	public void mouseReleased(MouseEvent e) { }
 
 	private synchronized void setKBD(boolean kbd) {
 		gkbd = kbd;
@@ -239,6 +431,7 @@ try {
 	// Calculator is sending hdr/data/ACK
 	// IOB is 2 or 3
 	public void do_dev(int _iob, int c) {
+		if (!devEna) return;
 		curIob = _iob;
 		bi_next(_iob, c); // process this byte, setup response
 		// fire back previously set up ACK or next
@@ -252,15 +445,25 @@ try {
 		curIob = _iob;
 		// must remain enabled unless IOB=0 or GROUP-2 addr mismatch
 		if (_iob == 0) {
-			if (debug && devEna) debugDump();
 			return devEna;
 		}
 		if ((_iob & 0b101) == 0b100) { // GROUP-1, ignore
 			return devEna;
 		}
-		devEna = ((c & ~15) == 0 && _devs[c].isSelected());
+		if ((c & ~15) != 0 || !_devs[c].isSelected()) {
+			// disable anything enabled...
+			if (curUnit >= 0) _leds[curUnit].set(false);
+			curUnit = -1;
+			devEna = false;
+		} else {
+			devEna = true; // just to be sure
+			if (c != curUnit && curUnit >= 0) {
+				_leds[curUnit].set(false);
+			}
+			curUnit = c;
+		}
 		if (!devEna) return devEna;
-		curUnit = c;
+		_leds[curUnit].set(true);
 		doGO();
 		return devEna;
 	}
@@ -304,6 +507,7 @@ System.err.format("do_ack\n");
 			mu.add(getMenu());
 		}
 		Wang_CN36_Bus.registerCN36(this);
+		onOff(true);
 	}
 	public void unPlug(JMenu mu) {
 		if (!plugged) return;
@@ -316,12 +520,15 @@ System.err.format("do_ack\n");
 			mu.remove(getMenu());
 		}
 		plugged = false;
+		onOff(false);
 	}
 	public boolean isPlugged() { return plugged; }
 
 	public void reset() { // hardware reset, a.k.a. PRIME
 		bi_hdr = 0;
 		devEna = false;
+		if (curUnit >= 0) _leds[curUnit].set(false);
+		curUnit = -1;
 	}
 	public JMenuItem getMenu() {
 		if (dev_mi != null) return dev_mi;
@@ -332,8 +539,13 @@ System.err.format("do_ack\n");
 
 	public JFrame getFrame() { return null; }
 	public Component getComponent() { return null; }
-	public void onOff(boolean vis) {}
-	public boolean onOff() { return false; }
+	public void onOff(boolean vis) {
+		if (visib != vis) {
+			visib = vis;
+			setVisible(vis);
+		}
+	}
+	public boolean onOff() { return visib; }
 	public void setProperties(Wang_Properties p) {}
 
 	public void run() {
